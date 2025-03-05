85ede3ecf9e3db12dde0cf08247863ee7bdaf86c
Sai Yan Naing <pesilay2372002@gmail.com>
2025-03-05 16:29:46 +0630

    nav animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/NavBar'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden ' >
+      <Navbar/>
       <Hero/>
       <About/>
     </main>
